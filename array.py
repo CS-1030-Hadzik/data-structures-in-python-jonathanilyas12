e9b3b41facbcfeb718120e69d160b4e5e993e9d7
jonathanilyas12 <117786596+jonathanilyas12@users.noreply.github.com>
2022-11-13 22:06:39 -0700

    Add files via upload

---
+++ array.py
@@ -0,0 +1,60 @@
+"""
+Name: Jonathan Ilyas
+CS1030
+CHAPTER 8 RESEARCH
+"""
+
+# problem 1
+print("Jonathan Ilyas")
+
+# problem 2.
+cars = ['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
+
+# problem 3.
+print(cars)
+
+#problem 4
+print(len(cars))
+
+
+#problem 5
+cars.append('Buick')
+
+#problem 6.
+print(cars)
+
+#problem 7.
+print(cars[3])
+
+#problem 8.
+cars.insert(3,'Toyota')
+
+#problem 9
+print(cars)
+
+#problem 10
+cars.pop(4)
+
+# problem 11
+print(cars)
+
+#problem 12
+cars.sort()
+
+#problem 13
+print(cars)
+
+#problem 14
+cars.sort(reverse=True)
+
+#problem 15
+print(cars)
+
+#problem 16
+my_array_length = len(cars)
+
+#problem 17
+array_string = ('The length of my array is')
+
+#problem 18
+print(array_string,'=',my_array_length)
